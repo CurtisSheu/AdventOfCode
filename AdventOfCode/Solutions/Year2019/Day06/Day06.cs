1dd7769dbe4c7a43d6272e5186e26e1657838f57
Curtis Sheu <sheu.curtis@gmail.com>
2019-12-09 16:51:11 -0500

    Update Day06.cs

--- AdventOfCode/Solutions/Year2019/Day06/Day06.cs
+++ AdventOfCode/Solutions/Year2019/Day06/Day06.cs
@@ -10,12 +10,79 @@ namespace AdventOfCode.Solutions.Year2019
 
         protected override string solvePartOne()
         {
-            return "";
+            string[] orbitList = Input.splitByNewLine();
+            List<Planet> innerPlanets = new List<Planet>();
+            foreach(string orbit in orbitList)
+            {
+                string[] planetList = orbit.Split(')');
+
+                Planet newInnerPlanet = null;
+                Planet newOuterPlanet = null;
+                foreach (Planet planet in innerPlanets)
+                {
+                    if (newInnerPlanet == null)
+                        newInnerPlanet = planet.getPlanetByName(planetList[0]);
+                    if (newOuterPlanet == null)
+                        newOuterPlanet = planet.getPlanetByName(planetList[1]);
+                }
+                if (newInnerPlanet == null)
+                {
+                    newInnerPlanet = new Planet(planetList[0]);
+                    innerPlanets.Add(newInnerPlanet);
+                }
+                if (newOuterPlanet == null)
+                    newOuterPlanet = new Planet(planetList[1]);
+                else if (innerPlanets.Contains(newOuterPlanet))
+                    innerPlanets.Remove(newOuterPlanet);
+
+                newInnerPlanet.addDirectOrbit(newOuterPlanet);
+            }
+            return innerPlanets[0].orbitCount().ToString();
         }
 
         protected override string solvePartTwo()
         {
             return "";
+        }
+    }
+
+    class Planet
+    {
+        private List<Planet> directOrbits;
+        public string name { get; }
+
+        public Planet(string n)
+        {
+            name = n;
+            directOrbits = new List<Planet>();
+        }
+
+        public int orbitCount()
+        {
+            int count = directOrbits.Count;
+            foreach (Planet p in directOrbits)
+                count += p.orbitCount();
+
+            return count;
+        }
+
+        public void addDirectOrbit(Planet p)
+        {
+            directOrbits.Add(p);
+        }
+
+        public Planet getPlanetByName(string planetToFind)
+        {
+            if (name == planetToFind)
+                return this;
+
+            foreach(Planet p in directOrbits)
+            {
+                var result = p.getPlanetByName(planetToFind);
+                if (result != null)
+                    return result;
+            }
+            return null;
         }
     }
 }
